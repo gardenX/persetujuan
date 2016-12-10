b922f9ffff1b7017ff24512adbabf6e98b282e71
Angga Firdaus <anggafirdaus916@gmail.com>
2016-12-10 14:38:41 +0700

    terbaru

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,13 +5,14 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Join With Us</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 
         <!-- Styles -->
         <style>
+        
             html, body {
                 background-color: #fff;
                 color: #636b6f;
@@ -20,6 +21,8 @@
                 height: 100vh;
                 margin: 0;
             }
+
+
 
             .full-height {
                 height: 100vh;
@@ -62,11 +65,68 @@
             .m-b-md {
                 margin-bottom: 30px;
             }
+            .question{
+                color: black;
+            }
+            .button-joiner {
+                text-align: center;
+            }
+            .button {
+                position: relative;
+                background: currentColor;
+                border: 1px solid currentColor;
+                font-size: 1.1rem;
+                color: #D81900;
+                margin: 3rem 0;
+                padding: 0.75rem 3rem;
+                cursor: pointer;
+                -webkit-transition: background-color 0.28s ease, color 0.28s ease, box-shadow 0.28s ease;
+                transition: background-color 0.28s ease, color 0.28s ease, box-shadow 0.28s ease;
+                overflow: hidden;
+                box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 3px 1px -2px rgba(0, 0, 0, 0.2), 0 1px 5px 0 rgba(0, 0, 0, 0.12);
+            }
+            .button span {
+                color: #fff;
+                position: relative;
+                z-index: 1;
+            }
+            .button::before {
+                content: '';
+                position: absolute;
+                background: #071017;
+                border: 50vh solid #1d4567;
+                width: 30vh;
+                height: 30vh;
+                border-radius: 50%;
+                display: block;
+                top: 50%;
+                left: 50%;
+                z-index: 0;
+                opacity: 1;
+                -webkit-transform: translate(-50%, -50%) scale(0);
+                transform: translate(-50%, -50%) scale(0);
+            }
+            .button:hover {
+                color: #D81900;
+                box-shadow: 0 6px 10px 0 rgba(0, 0, 0, 0.14), 0 1px 18px 0 rgba(0, 0, 0, 0.12), 0 3px 5px -1px rgba(0, 0, 0, 0.2);
+            }
+            .button:active::before, .button:focus::before {
+                -webkit-transition: opacity 0.28s ease 0.364s, -webkit-transform 1.12s ease;
+                transition: opacity 0.28s ease 0.364s, -webkit-transform 1.12s ease;
+                transition: transform 1.12s ease, opacity 0.28s ease 0.364s;
+                transition: transform 1.12s ease, opacity 0.28s ease 0.364s, -webkit-transform 1.12s ease;
+                -webkit-transform: translate(-50%, -50%) scale(1);
+                transform: translate(-50%, -50%) scale(1);
+                opacity: 0;
+            }
+            .button:focus {
+                outline: none;
+            }
         </style>
     </head>
     <body>
         <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
+        @if (Route::has('login'))
                 <div class="top-right links">
                     @if (Auth::check())
                         <a href="{{ url('/home') }}">Home</a>
@@ -76,18 +136,14 @@
                     @endif
                 </div>
             @endif
-
             <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                <div class="title m-b-md">Selamat Datang !</div>
+                <p class="question">Apakah Anda Ingin Bergabung ?</p>
+                <div class="joined">
+                    <div class="button-joider">
+                        <button type="submit" class="button"><span>New Token</span></button>
+                        <a href="https://facebook.com"><button type="submit" class="button"><span>Lanjutkan</span></button></a>
+                    </div>
                 </div>
             </div>
         </div>
